22ff0d62c3cf155a795ede79cc02ae760345e7b1
enero70 <stivenmoreta.c@gmail.com>
2022-03-06 22:01:50 -0300

    Updates

---
+++ precache-manifest.f4f44d5f3550c0024addfe72fb161963.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eef7728098115f9d15f209553723e687",
+    "url": "/To-Do-React-FrontEnd/index.html"
+  },
+  {
+    "revision": "355b6ef251c92aa51a54",
+    "url": "/To-Do-React-FrontEnd/static/css/main.69b16a98.chunk.css"
+  },
+  {
+    "revision": "d7779f595c43eb1f1f62",
+    "url": "/To-Do-React-FrontEnd/static/js/2.c4a38e97.chunk.js"
+  },
+  {
+    "revision": "8e99104a3a78d57e4af1",
+    "url": "/To-Do-React-FrontEnd/static/js/3.fac2528f.chunk.js"
+  },
+  {
+    "revision": "355b6ef251c92aa51a54",
+    "url": "/To-Do-React-FrontEnd/static/js/main.9dedc4d5.chunk.js"
+  },
+  {
+    "revision": "66a15d0d1142170dd5c6",
+    "url": "/To-Do-React-FrontEnd/static/js/runtime~main.b2939e60.js"
+  }
+]);
